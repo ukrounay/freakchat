61c07dbe3b357d23b5aaccc620dd0e3be828df00
Ruslan Nedolia <87966286+ukrounay@users.noreply.github.com>
2025-07-26 19:31:24 +0200

    fix connection

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ wss.on('connection', (ws) => {
   console.log('Client connected');
   
   ws.on('message', (message) => {
-    // Broadcast to everyone
+
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
